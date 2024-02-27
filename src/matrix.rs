616992505c83fa331a234ee70596b0465e9da405
Ossi Herrala <oherrala@iki.fi>
2024-02-27 16:19:24 +0200

    Update dependencies

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,9 +1,10 @@
 use std::io;
 
+use futures::stream::StreamExt;
 use matrix_sdk::config::SyncSettings;
+use matrix_sdk::matrix_auth::{MatrixSession, MatrixSessionTokens};
 use matrix_sdk::ruma::events::room::message::RoomMessageEventContent;
-use matrix_sdk::ruma::TransactionId;
-use matrix_sdk::{Client, Session};
+use matrix_sdk::{Client, SessionMeta};
 use tokio::sync::mpsc::UnboundedReceiver;
 use tokio::task::JoinHandle;
 use tracing::instrument;
@@ -17,14 +18,18 @@ pub async fn matrix_init(
 ) -> anyhow::Result<Vec<JoinHandle<io::Result<()>>>> {
     let client = Client::new(config.homeserver.clone()).await?;
 
-    let session = Session {
-        access_token: config.access_token.to_owned(),
-        refresh_token: None,
-        user_id: config.user_id.to_owned(),
-        device_id: config.device_id.to_owned(),
+    let session = MatrixSession {
+        meta: SessionMeta {
+            user_id: config.user_id.to_owned(),
+            device_id: config.device_id.to_owned(),
+        },
+        tokens: MatrixSessionTokens {
+            access_token: config.access_token.to_owned(),
+            refresh_token: None,
+        },
     };
 
-    client.restore_login(session).await?;
+    client.restore_session(session).await?;
 
     tracing::debug!("Doing first sync");
     if let Err(err) = client.sync_once(SyncSettings::default()).await {
@@ -34,13 +39,12 @@ pub async fn matrix_init(
 
     let mut handles = Vec::new();
     if let Ok(resp) = client.join_room_by_id(&config.room_id).await {
-        if let Some(room) = client.get_joined_room(&resp.room_id) {
+        if let Some(room) = client.get_room(resp.room_id()) {
             let handle = tokio::spawn(async move {
                 while let Some(line) = room_rx.recv().await {
-                    tracing::debug!("matrix tx: ^{line}$");
+                    tracing::info!("matrix tx: ^{line}$");
                     let content = RoomMessageEventContent::notice_plain(line);
-                    let txn_id = TransactionId::new();
-                    let resp = room.send(content, Some(&txn_id)).await;
+                    let resp = room.send(content).await;
                     tracing::debug!("Room message send response: {resp:?}");
                 }
                 Ok(())
@@ -50,10 +54,20 @@ pub async fn matrix_init(
     }
 
     let handle = tokio::spawn(async move {
-        client
-            .sync(SyncSettings::default())
-            .await
-            .map_err(|err| io::Error::new(io::ErrorKind::Interrupted, err))
+        let mut sync_stream = Box::pin(client.sync_stream(SyncSettings::default()).await);
+        while let Some(res) = sync_stream.next().await {
+            match res {
+                Ok(_) => (),
+                Err(err) => {
+                    tracing::error!("sync_stream returned error: {err}");
+                    return Err(io::Error::new(io::ErrorKind::Interrupted, err));
+                }
+            }
+        }
+        Err(io::Error::new(
+            io::ErrorKind::Interrupted,
+            "sync_stream died",
+        ))
     });
     handles.push(handle);
 
